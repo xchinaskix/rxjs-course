b85a50e1b811ae6f3562bd38d85f63ec9e7c55e5
xchinaskix <jglushko93@gmail.com>
2018-12-18 00:58:43 +0200

    lesson #28 done

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -11,11 +11,13 @@ import {
     concatMap,
     switchMap,
     withLatestFrom,
-    concatAll, shareReplay
+    concatAll, shareReplay, throttleTime
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservabes } from '../common/util';
+import { RxJsLoggingLevel, debug, setRxJsLoggingLevel } from '../common/debug';
+
 
 
 @Component({
@@ -35,7 +37,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservabes(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservabes(`/api/courses/${this.courseId}`)
+        .pipe(
+            // tap(course => console.log(course))
+            debug( RxJsLoggingLevel.INFO, 'course value'),
+        );
+
+        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
     }
 
     ngAfterViewInit() {
@@ -43,9 +51,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
         .pipe(
             map(event => event.target.value),
             startWith(''),
+            debug( RxJsLoggingLevel.TRACE, 'search'),
             debounceTime(400),
             distinctUntilChanged(),
-            switchMap(search => this.loadLessons(search))
+            switchMap(search => this.loadLessons(search)),
+            debug( RxJsLoggingLevel.INFO, 'lessons value'),
+
         );
     }
 

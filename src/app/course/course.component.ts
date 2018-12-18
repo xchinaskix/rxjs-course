e6f2f0b6b598b0fadd4752cc6ceb3f3e85ab34a5
xchinaskix <jglushko93@gmail.com>
2018-12-18 10:17:01 +0200

    operators part done

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,7 +13,7 @@ import {
     withLatestFrom,
     concatAll, shareReplay, throttleTime
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {merge, fromEvent, Observable, concat, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservabes } from '../common/util';
 import { RxJsLoggingLevel, debug, setRxJsLoggingLevel } from '../common/debug';
@@ -37,25 +37,35 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservabes(`/api/courses/${this.courseId}`)
+        this.course$ = createHttpObservabes(`/api/courses/${this.courseId}`);
+        // .pipe(
+        //     // tap(course => console.log(course))
+        //     debug( RxJsLoggingLevel.INFO, 'course value'),
+        // );
+        this.lessons$ = this.loadLessons();
+        forkJoin(this.course$, this.lessons$)
         .pipe(
-            // tap(course => console.log(course))
-            debug( RxJsLoggingLevel.INFO, 'course value'),
-        );
-
-        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+            tap(([course, lessons]) => {
+                console.log('course :', course);
+                console.log('lessons :', lessons);
+            })
+        )
+        .subscribe();
+        // setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
     }
 
     ngAfterViewInit() {
-        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+        // this.lessons$ =
+        fromEvent<any>(this.input.nativeElement, 'keyup')
         .pipe(
             map(event => event.target.value),
-            startWith(''),
-            debug( RxJsLoggingLevel.TRACE, 'search'),
-            debounceTime(400),
-            distinctUntilChanged(),
-            switchMap(search => this.loadLessons(search)),
-            debug( RxJsLoggingLevel.INFO, 'lessons value'),
+            throttleTime(500)
+            // startWith(''),
+            // debug( RxJsLoggingLevel.TRACE, 'search'),
+            // debounceTime(400),
+            // distinctUntilChanged(),
+            // switchMap(search => this.loadLessons(search)),
+            // debug( RxJsLoggingLevel.INFO, 'lessons value'),
 
         );
     }
